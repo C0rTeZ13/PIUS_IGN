029be88baa7f4cd726aa237b0ef3d52f38da7738
C0rTeZ13 <work.cortez13@gmail.com>
2024-03-14 20:43:19 +0300

    hooks test

--- database/migrations/2024_03_14_163048_create_admins_table.php
+++ database/migrations/2024_03_14_163048_create_admins_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->string('admins')->primary();
-
             $table->timestamps();
         });
     }
